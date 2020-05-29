c038580b995ddd7b0627ce2941b7d825b6092411
Dalton <C.Dalton@mitsui.com>
2020-05-29 16:51:09 -0500

    Built first material ui dev app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar'
+import CourseList from './components/CourseList'
 
 function App() {
   return (
     <div>
       <NavBar />
+      <CourseList />
     </div>
   );
 }
